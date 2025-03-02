b9bba400b3e7b86457dcfc3be17e9a8b22a9cd62
kobaken <kentafly88@gmail.com>
2025-03-02 11:43:16 +0900

    Update

--- lib/My/PriceCalculator.pm
+++ lib/My/PriceCalculator.pm
@@ -10,18 +10,20 @@ our @EXPORT_OK = qw(
 );
 
 use List::Util qw(min);
+use Result::Simple qw(Ok Err);
 
 # 割引率の上限
 use constant MAX_DISCOUNT_PERCENTAGE => 80;
 
 # 販売価格を計算する
-sub selling_price($sale_product, $user, $discount) {
-    my $total_discount_percentage = total_discount_percentage($user, $discount);
-    return floor($sale_product->price * (100 - $total_discount_percentage) / 100)
+sub selling_price($sale_product, $user, $discount, $coupon) {
+    my ($total_discount_percentage, $err) = total_discount_percentage($user, $discount);
+    return Err($err) if $err;
+    return Ok(floor($sale_product->price * (100 - $total_discount_percentage) / 100))
 }
 
 # 適用される割引率を計算する
-sub total_discount_percentage($user, $discount) {
+sub total_discount_percentage($user, $discount, $coupon) {
     my $percentage = 0;
     if ($user->is_premium) {
         $percentage += 10
@@ -29,5 +31,7 @@ sub total_discount_percentage($user, $discount) {
     if ($discount->percentage > 0) {
         $percentage += $discount->percentage;
     }
-    return min($percentage, MAX_DISCOUNT_PERCENTAGE)
+    # TODO: クーポンの割引率を加算する
+    # TODO: クーポンと$discountの併用を使用としていた場合は、Err('クーポンと割引は併用できません')を返す
+    return Ok(min($percentage, MAX_DISCOUNT_PERCENTAGE));
 }
